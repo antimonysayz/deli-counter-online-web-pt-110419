6876d3278e677c7743508dc9ac6a4be821c65854
Anthony Mai <anthony.mai530@gmail.com>
2019-11-10 03:06:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+  katz_deli = []
+  
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index|
+    message += " #{index.to_i + 1}. #{value}"
+  end
+    puts "#{message}"
+    end
+end
+
+ def take_a_number(array, name)
+   array << "#{name}"
+   puts "Welcome #{name}. You are currently number #{"
+ end
